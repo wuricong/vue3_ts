cf5fcc6838df64e5e3f78dd42c5a0b3db4400bf0
wuricong-es-prod <15070301734@163.com>
2024-06-03 20:46:03 +0800

    feat:添加element-plus

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import TheWelcome from './components/TheWelcome.vue'
       <HelloWorld msg="You did it!" />
     </div>
   </header>
-
+  <el-button type="primary">/</el-button>
   <main>
     <TheWelcome />
   </main>
